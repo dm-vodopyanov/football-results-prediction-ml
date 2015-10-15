ddb63f64b92970a55f7bea83feda6f66c7536dc9
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2015-10-15 22:41:01 +0300

    added TODOs;

--- src/parse.py
+++ src/parse.py
@@ -6,7 +6,7 @@
     To use it, you should call getInfo(teamName) function.
     It returns the list with following structure:
         [TeamName, OpponentTeamName, TeamNameScore, OpponentTeamNameScore, isTeamHome].
-    If team played the match at the home stadium, it returns True, otherwise - False.
+    If team played the match at the home stadium, isTeamHome returns True, otherwise - False.
 """
 
 import os
@@ -15,25 +15,30 @@ import csv
 dir = "..\\datasets"
 teamName = ""
 teamsStat = []
+matchesNumber = 0
 
-def getInfo(teamName):
+def getInfo(teamName, matchesNumber):
     if teamName == "":
         teamName = "Arsenal"
-    takeCSV(teamName)
+    if matchesNumber == 0:
+        matchesNumber = 20
+    takeCSV(teamName, matchesNumber)
+    #TODO: Take matchesNumber best results in all list
 
-def takeCSV(teamName):
+
+def takeCSV(teamName, matchesNumber):
     for name in os.listdir(dir):
         path = os.path.join(dir, name)
         if os.path.isfile(path):
             if name.endswith("csv"):
-                parseCSV(path, teamName)
+                parseCSV(path, teamName, matchesNumber)
         else:
             takeCSV(dir)
 
-def parseCSV(path, teamName):
+def parseCSV(path, teamName, matchesNumber):
     inputFile = open(path, "rb")
     rdr = csv.reader(inputFile)
-    for rec in rdr:
+    for rec in rdr: 
         try:
             isTeamHome = rec[2] == teamName
             isTeamAway = rec[3] == teamName
@@ -41,11 +46,12 @@ def parseCSV(path, teamName):
                 teamsStat.append([rec[2],rec[3],rec[4],rec[5],isTeamHome])
             elif isTeamAway:
                 teamsStat.append([rec[3],rec[2],rec[5],rec[4],isTeamHome])
-         except:
-             pass
+            #TODO: Add another info about teams
+        except:
+            pass
     inputFile.close()
 
 if __name__ == '__main__':
-    getInfo(teamName)
+    getInfo(teamName, matchesNumber)
     for team in teamsStat:
         print team
