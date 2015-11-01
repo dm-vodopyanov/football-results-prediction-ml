7ac12516a4f1448c8f39b803f37a0107aa987567
Dmitry Vodopyanov <dmitryvodop@gmail.com>
2015-11-01 18:30:33 +0300

    added teams significance and fixed all-teams parsing;

--- src/parse.py
+++ src/parse.py
@@ -5,7 +5,8 @@
 
     To use it, you should call getInfo(teamName) function.
     It returns the list with following structure:
-        [TeamName, OpponentTeamName, TeamNameScore, OpponentTeamNameScore, isTeamHome].
+        [TeamName, OpponentTeamName, TeamNameScore, OpponentTeamNameScore, 
+         TeamNameSignificance, OpponentTeamNameSignificanceisTeamNameHome].
     If team played the match at the home stadium, isTeamHome returns True, otherwise - False.
 """
 
@@ -13,53 +14,71 @@ import os
 import csv
 
 dir = "..\\datasets"
-resDir = "..\\result"
+txtDir = "..\\txt"
 teamName = ""
+teams = []
+teamSignificance = ""
+opponentTeamSignificance = ""
 teamsStat = []
-matchesNumber = 0
+matchesNumber = 40
 
-def getInfo(teamName, matchesNumber):
-    if teamName == "":
-        teamName = "Arsenal"
-    if matchesNumber == 0:
-        matchesNumber = 20
-    takeCSV(teamName, matchesNumber)
-    #TODO: Take matchesNumber best results in all list
+def getTeamsNamesList():
+    teamsFile = open(dir + "\\allTeams.txt", 'r')
+    for team in teamsFile:
+        teams.append(team.split(","))
+    teamsFile.close();
 
+def getInfo(teams, matchesNumber):
+    for team in teams:
+        if team[1].endswith("\n"):
+            team[1] = team[1][:team[1].find("\n")]
+        teamName = team[0]
+        teamSignificance = team[1]
+        getCSV(teamName, teamSignificance, matchesNumber)
+        del teamsStat[:]
 
-def takeCSV(teamName, matchesNumber):
+def getCSV(teamName, teamSignificance, matchesNumber):
     for name in os.listdir(dir):
         path = os.path.join(dir, name)
         if os.path.isfile(path):
             if name.endswith("csv"):
-                parseCSV(path, teamName, matchesNumber)
+                parseCSV(path, teamName, teamSignificance, matchesNumber)
         else:
-            takeCSV(dir)
+            getCSV(dir)
+        saveResults(teamsStat, teamName)
 
-def parseCSV(path, teamName, matchesNumber):
+def parseCSV(path, teamName, teamSignificance, matchesNumber):
     inputFile = open(path, "rb")
     rdr = csv.reader(inputFile)
     for rec in rdr: 
         try:
-            isTeamHome = rec[2] == teamName
-            isTeamAway = rec[3] == teamName
-            if isTeamHome:
-                teamsStat.append([rec[2],rec[3],rec[4],rec[5],str(isTeamHome)])
-            elif isTeamAway:
-                teamsStat.append([rec[3],rec[2],rec[5],rec[4],str(isTeamHome)])
-            #TODO: Add another info about teams
+            if (len(teamsStat) <= matchesNumber):
+                isTeamHome = rec[2] == teamName
+                isTeamAway = rec[3] == teamName
+                for team in teams:
+                    if isTeamHome:
+                        if (team[0] == rec[3]):
+                            opponentTeamSignificance = team[1]
+                    if isTeamAway:
+                        if (team[0] == rec[2]):
+                            opponentTeamSignificance = team[1] 
+                opponentTeamSignificance = opponentTeamSignificance[:opponentTeamSignificance.find("\n")]
+                if isTeamHome:
+                    teamsStat.append([rec[2], rec[3], rec[4], rec[5], 
+                                      teamSignificance, opponentTeamSignificance, str(isTeamHome)])
+                elif isTeamAway:
+                    teamsStat.append([rec[3], rec[2], rec[5], rec[4],
+                                      teamSignificance, opponentTeamSignificance, str(isTeamHome)])
         except:
             pass
     inputFile.close()
 
-def saveResults(results):
-    resFile = open(resDir + "\\result.txt", 'w')
-    for result in results:
-        resFile.write(';'.join(result) + '\n')
+def saveResults(teamsStat, teamName):
+    resFile = open(txtDir + "\\sortedData" + teamName.replace(" ","_") + ".txt", 'w')
+    for stat in teamsStat:
+        resFile.write(';'.join(stat) + '\n')
     resFile.close()
 
 if __name__ == '__main__':
-    getInfo(teamName, matchesNumber)
-    for team in teamsStat:
-        print team
-    saveResults(teamsStat)
+    getTeamsNamesList()
+    getInfo(teams, matchesNumber)
